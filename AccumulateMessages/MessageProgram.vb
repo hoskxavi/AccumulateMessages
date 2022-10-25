d6d06bdd90215a3c2b6dccbf6cbdef026a97cf81
hoskxavi <hoskmunch@gmail.com>
2022-10-25 08:11:01 -0600

    Add accumulation function to code.

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,15 +1,31 @@
 
 Imports System
+Imports System.Threading
 
 Module MessageProgram
     Sub Main(args As String())
         'uncomment to test interactively
-        'Test.Manual()
+        Test.Manual()
         Test.Auto()
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
         'your code here
+        Dim messages As String
+        Static oldMessage As String 'static variable retains it's value
+
+        If clear = False Then
+            If newMessage = "" Then
+                messages = oldMessage
+            Else
+                oldMessage &= newMessage + vbCrLf
+                messages = oldMessage
+            End If
+        Else  'clears messages when the "CLEAR" command is given
+                oldMessage = ""
+            messages = oldMessage
+        End If
+
         Return messages
     End Function
 
